7bc0d087d31bc8f8d8dbcf4e176f54b2c4b98caf
Michal Migurski <mike@teczno.com>
2014-02-25 16:35:45 -0800

    Nuked tasks.build_user()

--- tasks.py
+++ tasks.py
@@ -13,26 +13,6 @@ BUCKET = os.environ['S3_BUCKET']
 GITHUB = 'https://api.github.com'
 GITHUB_TOKEN = os.environ['GITHUB_TOKEN']
 
-def build_user(user):
-    user_info = {}
-    user_info['login'] = user.keys()[0]
-    repos = user.values()[0]
-    user_info['repositories'] = len(repos)
-    try:
-        user_info['contributions'] = sum([c['contributions'] for c in repos])
-    except KeyError:
-        pass
-    user_info['avatar_url'] = repos[0]['avatar_url']
-    user_info['html_url'] = repos[0]['html_url']
-    headers = {'Authorization': 'token %s' % GITHUB_TOKEN}
-    user_details = requests.get('%s/users/%s' % (GITHUB, user_info['login']), headers=headers)
-    if user_details.status_code == 200:
-        user_info['name'] = user_details.json().get('name')
-        user_info['company'] = user_details.json().get('company')
-        user_info['blog'] = user_details.json().get('blog')
-        user_info['location'] = user_details.json().get('location')
-    return user_info
-
 def update_issues(project_url):
     full_name = '/'.join(urlparse(project_url).path.split('/')[1:3])
     url = '%s/repos/%s/issues' % (GITHUB, full_name)
