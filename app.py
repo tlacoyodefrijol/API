70c796a209b96b310ca36f86c3fd2eb03355f7ea
Andrew Hyder <hyd415@gmail.com>
2014-03-19 01:39:27 -0700

    Added events api endpoint

--- app.py
+++ app.py
@@ -178,6 +178,7 @@ kwargs = dict(methods=['GET'], exclude_columns=['keep'], max_results_per_page=-1
 manager.create_api(Organization, collection_name='organizations', **kwargs)
 manager.create_api(Story, collection_name='stories', **kwargs)
 manager.create_api(Project, collection_name='projects', **kwargs)
+manager.create_api(Event, collection_name='events', **kwargs)
 
 # -------------------
 # Routes
